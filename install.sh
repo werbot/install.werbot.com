204731ce3327e545da766a5715136ab3c34be927
Dmitry Shurco <dmitry@werbot.com>
2022-09-25 10:50:40 +0000

    second configuration

--- install.sh
+++ install.sh
@@ -10,16 +10,18 @@ set -u
 
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 
-OS=$(uname -s)
-case "$OS" in
-Linux) OS=linux ;;
-Darwin) OS=darwin ;;
-*) err "Error: unsupported operating system: $OS" ;;
-esac
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
 
-install() {
+print_header() {
+  printf "%.45s " "$@ ........................................"
+}
+
+hello() {
   echo "${RED}"
   echo " _    _  ____  ____  ____  _____  ____"
   echo "( \\/\\/ )( ___)(  _ \\(  _ \\(  _  )(_  _)"
@@ -28,29 +30,94 @@ install() {
   echo "${RESET}"
   echo "Install Enterprise version"
   echo "------------------------------------------------"
+}
 
-  # install docker
+check_and_install() {
+  local OS
+  local CPU
+
+  # Checking operating system
+  print_header "Checking operating system"
+  OS=$(uname -s)
+  case "$OS" in
+  Linux) OS=linux ;;
+  Darwin) OS=darwin ;;
+  *) err "${RED}NOT SUPPORTED${RESET}" ;;
+  esac
+  echo "${GREEN}OK${RESET}"
+
+  # Checking CPU architecture
+  print_header "Checking CPU architecture"
+  CPU=$(uname -m)
+  case "$CPU" in
+  x86_64 | x86-64 | x64 | amd64) CPU=amd64 ;;
+  *) err "${RED}NOT SUPPORTED${RESET}" ;;
+  esac
+  echo "${GREEN}OK${RESET}"
+
+  # Installing jq
+  print_header "Checking install jq"
+  command -v jq >/dev/null 2>&1 || {
+    echo "${YELLOW}INSTALLATION${RESET}"
+    print_header "Installing jq"
+    if [ "$OS" = darwin ]; then
+      brew install jq >/dev/null 2>&1
+    elif [ "$OS" = linux ]; then
+      sudo curl -L https://github.com/stedolan/jq/releases/download/$(curl -s "https://api.github.com/repos/stedolan/jq/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/jq-linux64 -o /usr/local/bin/jq >/dev/null 2>&1
+      sudo chmod +x /usr/local/bin/jq >/dev/null 2>&1
+    fi
+    command -v jq >/dev/null 2>&1 || {
+      err "${RED}ERROR${RESET}"
+    }
+  }
+  echo "${GREEN}OK${RESET}"
+
+  # Installing docker
+  print_header "Checking install docker"
   command -v docker >/dev/null 2>&1 || {
+    echo "${YELLOW}INSTALLATION${RESET}"
+    print_header "Installing docker"
     if [ "$OS" = darwin ]; then
-      echo "install docker on Darwin"
+      brew install docker >/dev/null 2>&1
     elif [ "$OS" = linux ]; then
-      sudo apt install apt-transport-https ca-certificates curl software-properties-common &&
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
-        sudo apt-get update &&
-        sudo apt-get install -y docker-ce
+      curl -sSf https://get.docker.com | sh >/dev/null 2>&1
     fi
+    command -v docker >/dev/null 2>&1 || {
+      err "${RED}ERROR${RESET}"
+    }
   }
+  echo "${GREEN}OK${RESET}"
 
-  # install docker-compose
+  # Installing docker-compose
+  print_header "Checking install docker-compose"
   command -v docker-compose >/dev/null 2>&1 || {
+    echo "${YELLOW}INSTALLATION${RESET}"
+    print_header "Installing docker-compose"
     if [ "$OS" = darwin ]; then
-      echo "install docker-compose on Darwin"
+      brew install docker-compose >/dev/null 2>&1
     elif [ "$OS" = linux ]; then
-      sudo curl -L https://github.com/docker/compose/releases/download/$(curl -s "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose &&
-        sudo chmod +x /usr/local/bin/docker-compose
+      sudo curl -L https://github.com/docker/compose/releases/download/$(curl -s "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose >/dev/null 2>&1
+      sudo chmod +x /usr/local/bin/docker-compose >/dev/null 2>&1
     fi
+    command -v docker-compose >/dev/null 2>&1 || {
+      err "${RED}ERROR${RESET}"
+    }
   }
+  echo "${GREEN}OK${RESET}"
+}
+
+get_ip() {
+  local IP=$(ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1)
+  [ -z ${IP} ] && IP=$(curl -s https://ipv4.icanhazip.com)
+  [ -z ${IP} ] && IP=$(curl -s https://ipinfo.io/ip)
+  echo ${IP}
+}
+
+install() {
+  hello
+  check_and_install
+
+  get_ip
 }
 
 err() {
