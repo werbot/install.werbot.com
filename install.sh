08a2b93aafd1d2c6657b3e1170fe39d0cf368357
Dmitry Shurco <dmitry@werbot.com>
2022-09-25 12:39:45 +0000

    update

--- install.sh
+++ install.sh
@@ -17,109 +17,24 @@ command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
 
-success() {
+gen_password() {
+  echo $(tr -cd 'a-zA-Z0-9!#$%&()*+?@[]^_' </dev/urandom | fold -w 32 | head -n 1)
+}
+
+print_success() {
   echo "${COLOR_GREEN}SUCCESS${COLOR_RESET}" >&2
 }
 
-alert() {
+print_alert() {
   echo "${COLOR_YELLOW}$1${COLOR_RESET}" >&2
 }
 
-error() {
+print_error() {
   echo "${COLOR_RED}$1${COLOR_RESET}" >&2 && exit 1
 }
 
 print_header() {
   printf "%.45s " "$@ ........................................"
-}
-
-hello() {
-  echo "${COLOR_RED} _    _  ____  ____  ____  _____  ____"
-  echo "( \\/\\/ )( ___)(  _ \\(  _ \\(  _  )(_  _)"
-  echo " )    (  )__)  )   / ) _ < )(_)(   )("
-  echo "(__/\\__)(____)(_)\\_)(____/(_____) (__)"
-  echo "${COLOR_RESET}"
-  echo "Install Enterprise version"
-  echo "------------------------------------------------"
-}
-
-check_and_install() {
-  local OS
-  local CPU
-
-  # Checking operating system
-  print_header "Checking operating system"
-  OS=$(uname -s)
-  case "$OS" in
-  Linux) OS=linux ;;
-  Darwin) OS=darwin ;;
-  *) error "NOT SUPPORTED" ;;
-  esac
-  success
-  # ------------------------------------------------
-
-  # Checking CPU architecture
-  print_header "Checking CPU architecture"
-  CPU=$(uname -m)
-  case "$CPU" in
-  x86_64 | x86-64 | x64 | amd64) CPU=amd64 ;;
-  *) error "NOT SUPPORTED" ;;
-  esac
-  success
-  # ------------------------------------------------
-
-  # Installing jq
-  print_header "Checking install jq"
-  command_exists jq || {
-    alert "INSTALLATION"
-    print_header "Installing jq"
-    if [ "$OS" = darwin ]; then
-      brew install jq >/dev/null 2>&1
-    elif [ "$OS" = linux ]; then
-      sudo curl -L https://github.com/stedolan/jq/releases/download/$(curl -s "https://api.github.com/repos/stedolan/jq/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/jq-linux64 -o /usr/local/bin/jq >/dev/null 2>&1
-      sudo chmod +x /usr/local/bin/jq >/dev/null 2>&1
-    fi
-    command_exists jq || {
-      error "ERROR"
-    }
-  }
-  success
-  # ------------------------------------------------
-
-  # Installing docker
-  print_header "Checking install docker"
-  command_exists docker || {
-    alert "INSTALLATION"
-    print_header "Installing docker rootless"
-    if [ "$OS" = darwin ]; then
-      brew install docker >/dev/null 2>&1
-    elif [ "$OS" = linux ]; then
-      curl -sSf https://get.docker.com/rootless | sh >/dev/null 2>&1
-    fi
-    command_exists docker || {
-      error "ERROR"
-    }
-  }
-  success
-  # ------------------------------------------------
-
-  # Installing docker-compose
-  print_header "Checking install docker-compose"
-  command_exists docker-compose || {
-    alert "INSTALLATION"
-    print_header "Installing docker-compose"
-    if [ "$OS" = darwin ]; then
-      brew install docker-compose >/dev/null 2>&1
-    elif [ "$OS" = linux ]; then
-      sudo curl -L https://github.com/docker/compose/releases/download/$(curl -s "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose >/dev/null 2>&1
-      sudo chmod +x /usr/local/bin/docker-compose >/dev/null 2>&1
-    fi
-    command_exists docker-compose command_exists || {
-      error "ERROR"
-    }
-  }
-  success
-  # ------------------------------------------------
 }
 
 get_ip() {
@@ -130,8 +45,111 @@ get_ip() {
 }
 
 install() {
-  hello
-  check_and_install
+  echo "${COLOR_RED} _    _  ____  ____  ____  _____  ____"
+  echo "( \\/\\/ )( ___)(  _ \\(  _ \\(  _  )(_  _)"
+  echo " )    (  )__)  )   / ) _ < )(_)(   )("
+  echo "(__/\\__)(____)(_)\\_)(____/(_____) (__)"
+  echo "${COLOR_RESET}"
+  echo "Install Enterprise version"
+  echo "------------------------------------------------"
+
+  # Checking operating system
+  local OS
+  print_header "Checking operating system"
+  OS=$(uname -s)
+  case "$OS" in
+  Linux) OS=linux ;;
+  Darwin) OS=darwin ;;
+  *) print_error "NOT SUPPORTED" ;;
+  esac
+  print_success
+  # ------------------------------------------------
+
+  # Checking CPU architecture
+  local CPU
+  print_header "Checking CPU architecture"
+  CPU=$(uname -m)
+  case "$CPU" in
+  x86_64 | x86-64 | x64 | amd64) CPU=amd64 ;;
+  *) print_error "NOT SUPPORTED" ;;
+  esac
+  print_success
+  # ------------------------------------------------
+
+  # Installing jq
+  print_header "Checking install jq"
+  command_exists jq || {
+    print_alert "INSTALLATION"
+    print_header "Installing jq"
+    if [ "$OS" = darwin ]; then
+      brew install jq >/dev/null 2>&1
+    elif [ "$OS" = linux ]; then
+      sudo curl -L https://github.com/stedolan/jq/releases/download/$(curl -s "https://api.github.com/repos/stedolan/jq/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/jq-linux64 -o /usr/local/bin/jq >/dev/null 2>&1
+      sudo chmod +x /usr/local/bin/jq >/dev/null 2>&1
+    fi
+    command_exists jq || {
+      print_error "ERROR"
+    }
+  }
+  print_success
+  # ------------------------------------------------
+
+  # Installing docker
+  print_header "Checking install docker"
+  command_exists docker || {
+    print_alert "INSTALLATION"
+    print_header "Installing docker"
+    if [ "$OS" = darwin ]; then
+      brew install docker >/dev/null 2>&1
+    elif [ "$OS" = linux ]; then
+      curl -sSf https://get.docker.com | sh >/dev/null 2>&1
+    fi
+    command_exists docker || {
+      print_error "ERROR"
+    }
+  }
+  print_success
+  # ------------------------------------------------
+
+  # Installing docker-compose
+  print_header "Checking install docker-compose"
+  command_exists docker-compose || {
+    print_alert "INSTALLATION"
+    print_header "Installing docker-compose"
+    if [ "$OS" = darwin ]; then
+      brew install docker-compose >/dev/null 2>&1
+    elif [ "$OS" = linux ]; then
+      sudo curl -L https://github.com/docker/compose/releases/download/$(curl -s "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose >/dev/null 2>&1
+      sudo chmod +x /usr/local/bin/docker-compose >/dev/null 2>&1
+    fi
+    command_exists docker-compose command_exists || {
+      print_error "ERROR"
+    }
+  }
+  print_success
+  # ------------------------------------------------
+
+  # Create new user
+  if ! getent passwd werbot >/dev/null; then
+    print_header "Adding a new werbot user"
+    sudo useradd -m -d /home/werbot werbot -s /bin/bash &>/dev/null
+    echo "werbot ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/werbot >/dev/null
+    sudo chmod 0440 /etc/sudoers.d/werbot
+    sudo usermod -a -G sudo werbot
+    sudo su - werbot -c "ssh-keygen -q -t ed25519 -N '' -f ~/.ssh/id_ed25519 <<<y" >/dev/null 2>&1
+    sudo usermod -aG docker werbot &>/dev/null
+    newgrp docker &>/dev/null
+    print_success
+  fi
+  # ------------------------------------------------
+
+  # Create structure service
+  print_header "Create structure service"
+  if ! [ -d /home/werbot/service ]; then
+    sudo su - werbot -c "mkdir /home/werbot/service"
+  fi
+  print_success
+  # ------------------------------------------------
 
   get_ip
 }
